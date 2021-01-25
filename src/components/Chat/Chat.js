4515618ec13098f71fb1ec293fcb77e11dbd1dd4
snowiewdev <snowiewongdev@gmail.com>
2021-01-25 13:21:37 +0800

    added dependency

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -45,7 +45,7 @@ const Chat = ({ location }) => {
     });
 
     //only when values inside [] changes, the useEffect will re-render
-  }, [location.search]);
+  }, [location.search, connectionOptions]);
 
   //handling messages
   useEffect(() => {
